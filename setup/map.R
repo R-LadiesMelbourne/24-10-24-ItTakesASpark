1090cc5d0211d54574f44f4e8012c946923c0078
kzeglinski <49138438+kzeglinski@users.noreply.github.com>
2024-10-22 16:34:29 +1100

    Update map.R to have URL to this repo

--- setup/map.R
+++ setup/map.R
@@ -1,2 +1,2 @@
-map <- load(url("https://raw.githubusercontent.com/shaziaruybal/2022-05-04-It-takes-a-spark/master/data/Map.rda"))
+map <- load(url("https://github.com/R-LadiesMelbourne/24-10-24-ItTakesASpark/blob/9c081da56864c00420ccb73984e578664393de40/data/Map.rda"))
 map_data <- read_csv("map_data.csv") %>% janitor::clean_names()
